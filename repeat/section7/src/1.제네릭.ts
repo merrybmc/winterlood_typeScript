fe2ca7650000b9de82f29315ea45fe32babd2fe0
Byeong Min Cho <merrybmc@gmail.com>
2023-12-18 22:11:44 +0900

    제네릭

--- repeat/section7/src/1.제네릭.ts
+++ repeat/section7/src/1.제네릭.ts
@@ -11,3 +11,5 @@ let num = func(10);
 let bool = func(true);
 let str = func('hello');
 let arr = func([1, 2, 3]);
+let tuple = func([1, 2, 3] as [number, number, number]); // 타입 단언
+let tuple2 = func<[number, number, number]>([1, 2, 3]);
