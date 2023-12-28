a9863f2865803b5e5f9ad4ebf8b11a5688774b70
Byeong Min Cho <merrybmc@gmail.com>
2023-12-28 21:39:01 +0900

    맵드 유틸리티 타입

--- repeat/section10/src/1.맵드 유틸리티 타입.ts
+++ repeat/section10/src/1.맵드 유틸리티 타입.ts
@@ -1,3 +1,4 @@
+import { fchown } from 'fs';
 /**
  * Partial<T>
  * -> 부분적인, 일부분의
@@ -26,9 +27,31 @@ type makePartial<T> = {
  * -> 특정 객체 타입의 모든 프로퍼티를 필수 프로퍼티로 바꿔주는 타입
  */
 
+type Required<T> = {
+  [key in keyof T]-?: T[key]; // -? : 부분 프로퍼티 제거
+};
+
 const withThumbnailPost: Required<Post> = {
   title: '한 입 타스 후기',
   tags: ['ts'],
   content: '',
   thumbnailURL: '',
 };
+
+/**
+ * Readonly<T>
+ * -> 읽기전용 수정불가
+ * -> 특정 객체 타입에서 모든 프로퍼티를 읽기 전용 프로퍼티로 만들어주는 타입
+ */
+
+type Readonly<T> = {
+  readonly [key in keyof T]: T[key];
+};
+
+const readonlyPost: Readonly<Post> = {
+  title: '보호된 게시글 입니다.',
+  tags: [],
+  content: '',
+};
+
+// readonlyPost.content = '';
